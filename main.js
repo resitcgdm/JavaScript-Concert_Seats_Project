fab7de1a1bce17e6f20ca12dfacf4e9266dc1825
Reşit <resitcgdm@gmail.com>
2022-12-23 17:09:32 +0300

    Added Comments

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-console.log(canSeeStage([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));  // True
-console.log(canSeeStage([[0, 0, 0], [1, 1, 1], [2, 2, 2]]));  // True
-console.log(canSeeStage([[2, 0, 0], [1, 1, 1], [2, 2, 2]]));  // False
-console.log(canSeeStage([[1, 0, 0], [1, 1, 1], [2, 2, 2]]));  // False
-
+// Displaying
+console.log(canSeeStage([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));  
+console.log(canSeeStage([[0, 0, 0], [1, 1, 1], [2, 2, 2]]));  
+console.log(canSeeStage([[2, 0, 0], [1, 1, 1], [2, 2, 2]]));  
+console.log(canSeeStage([[1, 0, 0], [1, 1, 1], [2, 2, 2]]));  
+ 
+// Creating Function
 function canSeeStage(seats) {
 
     for (let i = 0; i < seats.length; i++) {
